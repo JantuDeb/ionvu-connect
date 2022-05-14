75ff34849eb5ded6263d37e07023b29c71d25f7e
Jantu Deb <63020886+JantuDeb@users.noreply.github.com>
2022-05-14 12:57:17 +0530

    Update axios-config.js

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -1,4 +1,7 @@
 export const axiosConfig = {
   baseURL: "https://social-media-backendapi.herokuapp.com/api/v1/",
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials:true
 };
